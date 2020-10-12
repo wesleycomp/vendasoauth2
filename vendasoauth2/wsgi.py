ee9ca8168da270d12e1d292ae4249a0d38e3ac36
wesleycomp <weslleycomp@gmail.com>
2020-10-12 08:33:19 -0300

    corrigindo index

--- vendasoauth2/wsgi.py
+++ vendasoauth2/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendasoauth2.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendasoauth2.settings')
 
 #application = get_wsgi_application()
 application = Cling(MediaCling(get_wsgi_application()))
