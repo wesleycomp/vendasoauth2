138da0b02f508c30194e8a196827ef7e1d0cdcc9
wesleycomp <weslleycomp@gmail.com>
2020-10-12 14:58:09 -0300

    atualizando com whitenoise 4

--- vendasoauth2/settings/base.py
+++ vendasoauth2/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 from pathlib import Path
-import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -90,6 +90,8 @@ WSGI_APPLICATION = 'vendasoauth2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -100,7 +102,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
